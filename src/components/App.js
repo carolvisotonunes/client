09d9abe129373a396f388be7e7e506f4f42e3f65
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-07-07 17:10:28 +1000

    wiring up routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      {/* DO NOT USE A HEF ERASE ALL DATA CREATING A BRAND NEW HTML BROWSER*/}
-      <a href="/pagetwo">Navigate to Page Two</a>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click Me!</button>
-      {/* DO NOT USE A HEF ERASE ALL DATA CREATING A BRAND NEW HTML BROWSER*/}
-      <a href="/">Navigate to Page One</a>
-    </div>
-  );
-};
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
