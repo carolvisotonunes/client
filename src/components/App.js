bc4a0fc1168432c34af74be56c74c1f3d51eec40
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-07-07 15:38:43 +1000

    do not use a hef with react router

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      {/* DO NOT USE A HEF ERASE ALL DATA CREATING A BRAND NEW HTML BROWSER*/}
+      <a href="/pagetwo">Navigate to Page Two</a>
+    </div>
+  );
 };
 
 const PageTwo = () => {
@@ -10,6 +16,8 @@ const PageTwo = () => {
     <div>
       PageTwo
       <button>Click Me!</button>
+      {/* DO NOT USE A HEF ERASE ALL DATA CREATING A BRAND NEW HTML BROWSER*/}
+      <a href="/">Navigate to Page One</a>
     </div>
   );
 };
