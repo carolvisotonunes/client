c61cc20a321f9c559be289b0d8e4bc27fb828353
Caroline Visoto Nunes <carolvisoto@gmail.com>
2021-07-08 13:07:15 +1000

    wiring up the google api library

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '211121290479-71kpeh8crubaomg4ra4ectd49lopb9af.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
